401f89b70f5777b20f15e95cfd54b11fb48051bf
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-05-23 16:23:47 -0700

    feat: create new task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "yo yo",
+      day: "April 20 1996",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header title="Task Tracker"></Header>
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,18 +1,9 @@
-import { useState } from "react";
-
-function Tasks() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "yo yo",
-      day: "April 20 1996",
-      reminder: false,
-    },
-  ]);
+import Task from "./Task";
+function Tasks({ tasks }) {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </>
   );
