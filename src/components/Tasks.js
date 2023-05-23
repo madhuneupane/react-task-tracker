2c5a2986ba65ad9f8960e677a6a39e5e88f48922
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-05-23 15:45:42 -0700

    feat: iterate through tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function Tasks() {
-  return <div></div>;
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "yo yo",
+      day: "April 20 1996",
+      reminder: false,
+    },
+  ]);
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
 }
 
 export default Tasks;
