ba93bf710636009360445d2058ea586b3df923c4
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-05-23 19:06:29 -0700

    feat: fetch and delete from server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "yo yo",
-      day: "April 20 1996",
-      reminder: false,
-    },
-  ]);
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  //fetch tasks
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  };
 
   //Toggle Reminder
   const toggleReminder = (id) => {
@@ -23,7 +33,10 @@ function App() {
   };
   //Delete task
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "DELETE",
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
@@ -36,8 +49,12 @@ function App() {
 
   return (
     <div className="container">
-      <Header title="Task Tracker" />
-      <AddTask onAdd={addTask} />
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAddTask={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("Clicked");
-  };
+const Header = ({ title, onAdd, showAddTask }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <Button
+        color={showAddTask ? "red" : "green"}
+        text={showAddTask ? "close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
