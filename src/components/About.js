6a646fc982dfa3f7ca663a998e25b87ada01b681
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-05-30 16:53:30 -0700

    Refactor: change on about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 const About = () => {
   return (
     <div>
-      <h4>This Assignment belongs to Madhu Nyoupane</h4>
-      <h4>Version 1.0.0</h4>
+      <h4>This Assignment belongs to Madhu Nyoupane and Lais Alves</h4>
+      <h4>Version 1.1.0</h4>
       <p>
         Code Along from:{" "}
         <Link to="https://www.youtube.com/watch?v=w7ejDZ8SWv8">Click Here</Link>
